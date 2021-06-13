bf83741ab05be7007e438d7d779bca059dcf348f
VRivera <boritica@hotmail.com>
2021-06-13 12:14:07 -0400

    adding style to earthquake data

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -1,4 +1,4 @@
-// Mod 13.6.1 add earthquak data to map
+//Mod 13.6.2 add styel to earthquake data
 
 // Add console.log to check to see if our code is working.
 console.log("working");
@@ -38,10 +38,45 @@ let earthquake7days = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary
 
 // Grabbing our GeoJSON data.
 d3.json(earthquake7days).then(function(data) {
+
+  // This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
+function styleInfo(feature) {
+  return {
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: "#ffae42",
+    color: "#000000",
+    radius: getRadius(feature.properties.mag),
+    stroke: true,
+    weight: 0.5
+  };
+}
+// This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+function getRadius(magnitude) {
+  if (magnitude === 0) {
+    return 1;
+  }
+  return magnitude * 4;
+}
     console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
-});
+ 
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data, {
+
+  // We turn each feature into a circleMarker on the map.
+  
+  pointToLayer: function(feature, latlng) {
+              console.log(data);
+              return L.circleMarker(latlng);
+          },
+       // We set the style for each circleMarker using our styleInfo function.
+    style: styleInfo
+    
+      }).addTo(map);
+  });
 
 
 
